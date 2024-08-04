6a5a948d184b0d8f4f5d860a4190e08c5fee0064
RihamSb <156914547+RihamSb@users.noreply.github.com>
2024-08-04 18:25:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import ArtistSearch from './ArtistSearch';
-import ArtistPage from './ArtistPage'; 
+import ArtistPage from './ArtistPage';
+
+//check if token exists
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/" replace />;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/artist-search" element={<ArtistSearch />} />
-        <Route path="/artist/:id" element={<ArtistPage />} /> 
+        <Route
+          path="/artist-search"
+          element={
+            <PrivateRoute>
+              <ArtistSearch />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/artist/:id"
+          element={
+            <PrivateRoute>
+              <ArtistPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
